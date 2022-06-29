337976b3638c60d22f8c3e5574736715bafec90b
MiyataKouki <k021g1124@g.neec.ac.jp>
2022-06-29 09:46:45 +0900

    ライティング

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -4,6 +4,7 @@ texture2D<float4> tex:register(t0);
 SamplerState smp:register(s0);
 
 float4 main(VSOutput input) :SV_TARGET{
-	return float4(tex.Sample(smp,input.uv)*color);
+	return float4(input.normal,1); //RGB‚ğ‚»‚ê‚¼‚ê–@ü‚ÌXYZ,A‚ğ1‚Åo—Í
+	/*return float4(tex.Sample(smp,input.uv)*color);*/
 	/*return float4(1,1,1,1);*/
 }
--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -5,10 +5,11 @@ float4 main( float4 pos : POSITION ) : SV_POSITION
 	return pos;
 }
 
-VSOutput main(float4 pos:POSITION, float2 uv : TEXCOORD)
+VSOutput main(float4 pos:POSITION,float3 normal:NORMAL, float2 uv : TEXCOORD)
 {
 	VSOutput output; // ピクセルシェーダに渡す値
 	output.svpos = mul(mat, pos);
+	output.normal = normal;
 	output.uv = uv;
 	return output;
 }
