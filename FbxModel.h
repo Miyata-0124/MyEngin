72811b2a6d28784741700caa0d45ff9a65a98f21
MiyataKouki <k021g1124@g.neec.ac.jp>
2023-05-31 12:34:27 +0900

    FbxModelの追加

---
+++ FbxModel.h
@@ -0,0 +1,127 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <DirectXMath.h>
+#include <DirectXTex.h>
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <d3dx12.h>
+#include <fbxsdk.h>
+
+//ノード
+struct Node
+{
+	//名前
+	std::string name;
+	//ローカルスケール
+	DirectX::XMVECTOR scaling = { 1,1,1,0 };
+	//ローカル回転角
+	DirectX::XMVECTOR rotation = { 0,0,0,0 };
+	//ローカル座標
+	DirectX::XMVECTOR translation = { 0,0,0,1 };
+	//ローカル変形行列
+	DirectX::XMMATRIX transform;
+	//グローバル変形行列
+	DirectX::XMMATRIX globalTransform;
+
+	Node* parent = nullptr;
+};
+
+class FbxModel
+{
+public://定数
+	//ボーンインデックスの最大値
+	static const int MAX_BONE_INDICES = 4;
+public:
+	//頂点データ構造体
+	struct VertexPosNormalUvSkin
+	{
+		DirectX::XMFLOAT3 pos;//座標
+		DirectX::XMFLOAT3 normal;//ベクトル
+		DirectX::XMFLOAT2 uv;//座標
+		UINT boneIndex[MAX_BONE_INDICES];//ボーン,番号
+		float boneWeight[MAX_BONE_INDICES];//ボーン,重み
+	};
+	//ボーン構造体
+	struct Bone
+	{
+		//名前
+		std::string name;
+		//初期姿勢の逆行列
+		DirectX::XMMATRIX invInitialPose;
+		//クラスター(FBX側のボーン情報)
+		FbxCluster* fbxCluster;
+		//コンストラクタ
+		Bone(const std::string& name) {
+			this->name = name;
+		}
+	};
+
+	friend class FbxLoader;
+
+private:
+	//アンビエント係数
+	DirectX::XMFLOAT3 ambient = { 1,1,1 };
+	//ディフーズ係数
+	DirectX::XMFLOAT3 diffuse = { 1,1,1 };
+	//テクスチャメタデータ
+	DirectX::TexMetadata metadata = {};
+	//スクラッチイメージ
+	DirectX::ScratchImage scratchImag = {};
+
+	//メッシュを持つノード
+	Node* meshNode = nullptr;
+	//頂点データ配列
+	std::vector<VertexPosNormalUvSkin> vertices;
+	//頂点インデックス配列
+	std::vector<unsigned short> indices;
+
+	//モデル名
+	std::string name;
+	//ノード配列
+	std::vector<Node> nodes;
+	//ボーン配列
+	std::vector<Bone> bones;
+
+	//Microsoft::WRL::を省略する
+	template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
+	//DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+	using TexMetadata = DirectX::TexMetadata;
+	using ScratchImage = DirectX::ScratchImage;
+	//std::を省略
+	using string = std::string;
+	template <class T>using vector = std::vector<T>;
+	//頂点バッファ
+	ComPtr<ID3D12Resource> vertBuff;
+	//インデックスバッファ
+	ComPtr<ID3D12Resource> indexBuff;
+	//テクスチャバッファ
+	ComPtr<ID3D12Resource> texBuff;
+	//頂点バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vbView = {};
+	//インデックスバッファビュー
+	D3D12_INDEX_BUFFER_VIEW ibView = {};
+	//SRV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
+
+	//FBXシーン
+	FbxScene* fbxScene = nullptr;
+public://関数
+	FbxScene* GetFbxScene() { return fbxScene; }
+	//デストラクタ
+	~FbxModel();
+
+	std::vector<Bone>& GetBone() { return bones; }
+
+	//モデル変形行列取得
+	const XMMATRIX& GetModeltransform() { return meshNode->globalTransform; }
+	//バッファ生成
+	void CreateBuffers(ID3D12Device* device);
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+};
+
