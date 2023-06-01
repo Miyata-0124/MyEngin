3761b9326379f0e59a608633ecf55269a1221e71
MiyataKouki <k021g1124@g.neec.ac.jp>
2023-06-01 10:34:46 +0900

    場所変更

--- GameScene.cpp
+++ GameScene.cpp
@@ -2,18 +2,19 @@
 
 void GameScene::Initialize()
 {
-#pragma	region	基盤システムの初期化
 #pragma region WindowsAPIの初期化
-	
-	winApp = new WinApp;
+	winApp = new	WinApp;
 	winApp->Initialize();
 #pragma endregion
 
 #pragma region DirectX初期化処理
 	//DirectX初期化処理　　ここから
-	
 	directXCom = new DirectXCommon;
 	directXCom->Initialize(winApp);
+
+	//FBX関連
+	FbxLoader::GetInstance()->Initialize(directXCom->GetDevice());
+
 	//キー情報
 	input = new	Input;
 	input->Initialize(winApp);
@@ -24,7 +25,7 @@ void GameScene::Initialize()
 	//スプライト共通部分の初期化
 	spriteCommon = new SpriteCommon;
 	spriteCommon->Initialize(directXCom);
-	spriteCommon->Loadtexture(1, "testpar1.png");
+	spriteCommon->Loadtexture(1, "MK.png");
 	spriteCommon->Loadtexture(2, "test.png");
 #pragma	region	シーンの初期化
 	//ViewProjection
@@ -32,11 +33,21 @@ void GameScene::Initialize()
 //	camera->Initialeze();
 //	//一度しか宣言しない
 	Object3d::StaticInitialize(directXCom->GetDevice(), WinApp::window_width, WinApp::window_height);
+	FbxObject3d::StaticInitialize(directXCom->GetDevice(), WinApp::window_width, WinApp::window_height);
+
 	//スプライト
 	sprite->Initialize(spriteCommon, 1);
 	sprite->SetAnchorPoint(XMFLOAT2(0.5f, 0.5f));
-	sprite->SetSize(XMFLOAT2(96.0f, 96.0f));
-	sprite->SetPosition({ 48,48 });
+	sprite->SetSize(XMFLOAT2(320.0f, 180.0f));
+	sprite->SetPosition({ 160,90 });
+
+	model = FbxLoader::GetInstance()->LoadModelFromFile("boneTest");
+
+	object1->initialize();
+	object1->SetModel(model);
+	object1->SetRotation({ 0,60,0 });
+	object1->SetPosition({ 0,-3,0 });
+	object1->PlayAnimation();
 	//
 	//	Sprite* sprite2 = new Sprite();
 	//	sprite2->Initialize(spriteCommon, 2);
@@ -44,16 +55,16 @@ void GameScene::Initialize()
 	//	sprite2->SetSize(XMFLOAT2(100.0f, 100.0f));
 	//	sprite2->SetPosition({ 50,50 });
 	//	//モデル
-	//	Model* model = Model::LoadFromOBJ("sphere");
+	//	Model* model1 = Model::LoadFromOBJ("sphere");
 	//	Model* model2 = Model::LoadFromOBJ("wall");
-	//	//3dオブジェクト生成
+		//3dオブジェクト生成
 	//	Object3d* obj3d = Object3d::Create();
 	//	Object3d* obj3d2 = Object3d::Create();
 	//
-	//	//modelクラスをひも付け
-	//	obj3d->SetModel(model);
+		//modelクラスをひも付け
+	//	obj3d->SetModel(model1);
 	//	obj3d->SetSize({ 2,2,2 });
-	//	obj3d->SetPosition({ -15,0,0 });
+	//	obj3d->SetPosition({ 15,0,0 });
 	//
 	//	obj3d2->SetModel(model2);
 	//	obj3d2->SetSize({ 2,2,2 });
@@ -67,11 +78,15 @@ void GameScene::Initialize()
 	//	// 引数の数字はテクスチャ読み込みのインデックスナンバー
 	//	particle = Particle::Create(1);
 	//	particle->Update();
+	//#pragma	endregion
 }
 
 void GameScene::Update()
 {
+	///それぞれのクラスのUpdateのみ記述
+	//キー情報
 	input->Update();
+
 	//{
 	//	XMFLOAT3 eye = camera->GetEye();
 	//	XMFLOAT3 traget = camera->GetTarget();
@@ -124,27 +139,32 @@ void GameScene::Update()
 			sprite->SetPosition(position);
 		}*/
 #pragma endregion
-		/*obj3d->Update();
-		obj3d2->Update();
+		//obj3d->Update();
+		/*obj3d2->Update();
 		particle->Update();*/
 
+	object1->Update();
 }
 
 void GameScene::Draw()
 {
-	//Direct毎フレーム処理　ここから
+	//描画処理ここから↓
 	directXCom->PreDraw();
-	/*Object3d::PreDraw(directXCom->GetCommandList());
-	obj3d->Draw();
-	obj3d2->Draw();
+	//背景
 
-	Object3d::PostDraw();
-	Particle::PreDraw(directXCom->GetCommandList());
 
-	particle->Draw();
+	//オブジェクト
+	object1->Draw(directXCom->GetCommandList());
+	//Object3d::PreDraw(directXCom->GetCommandList());
+	//obj3d->Draw();
+	//obj3d2->Draw();
 
-	Particle::PostDraw();*/
+	//Object3d::PostDraw();
+	//Particle::PreDraw(directXCom->GetCommandList());
+	//particle->Draw();
+	//Particle::PostDraw();
 
+	// UI関連
 	sprite->SetIsInvisible(false);
 	sprite->SetTexIndex(1);
 	sprite->Draw();
@@ -152,17 +172,21 @@ void GameScene::Draw()
 	sprite2->Draw();*/
 
 	directXCom->PostDraw();
+	//ここまで↑
 }
 
 void GameScene::Finalize()
 {
 	winApp->Finalize();
+	FbxLoader::GetInstance()->Finalize();
 	delete input;
 	delete winApp;
 	delete directXCom;
 	delete spriteCommon;
 	delete sprite;
 	//delete model;
+	delete object1;
+	//delete model1;
 	//delete model2;
 	//delete obj3d;
 }
--- GameScene.h
+++ GameScene.h
@@ -8,28 +8,48 @@
 #include "Sprite.h"
 #include "Particle.h"
 #include "Object3d.h"
+#include "FbxObject3D.h"
 #include "Model.h"
 
-using namespace DirectX;
-using namespace Microsoft::WRL;
-
+#include "FbxLoader.h"
 class GameScene
 {
 public: //基本関数
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
 	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
 	void Draw();
+
+	/// <summary>
+	/// 解放
+	/// </summary>
 	void Finalize();
 
-public:
+public: //mainに一部引き渡し用
 	WinApp* GetWinApp() { return winApp; }
 	Input* GetInput() { return input; }
 
 private: //ゲーム内使用クラス
 	WinApp* winApp = nullptr;
 	DirectXCommon* directXCom = nullptr;
+	//キー情報
 	Input* input = nullptr;
+	//スプライト
 	SpriteCommon* spriteCommon = nullptr;
 	Sprite* sprite = new Sprite();
+	//FBX
+	FbxModel* model = nullptr;
+	FbxObject3d* object1 = new FbxObject3d();
+
 };
 
