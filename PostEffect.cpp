e3df97d3d8eb8d6aaab3ed6610d62064c7163692
MiyataKouki <k021g1124@g.neec.ac.jp>
2023-06-12 14:48:03 +0900

    PostEffect 途中

--- PostEffect.cpp
+++ PostEffect.cpp
@@ -1,4 +1,5 @@
 #include "PostEffect.h"
+#include "WinApp.h"
 #include <d3dx12.h>
 
 using namespace DirectX;
@@ -12,6 +13,69 @@ unsigned short postIndices[] = {
 PostEffect::PostEffect()
 	: Sprite()
 {
+}
+
+void PostEffect::initialize(DirectXCommon dxCommon)
+{
+	HRESULT result;
+
+	//テクスチャリソース設定
+	CD3DX12_RESOURCE_DESC texresDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+		DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
+		WinApp::window_width,
+		(UINT)WinApp::window_height,
+		1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET
+	);
+
+	//テクスチャバッファの生成
+	result = dxCommon.GetDevice()->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_CPU_PAGE_PROPERTY_WRITE_BACK, D3D12_MEMORY_POOL_L0),
+		D3D12_HEAP_FLAG_NONE,
+		&texresDesc,
+		D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+		nullptr,
+		IID_PPV_ARGS(&texBuff));
+	assert(SUCCEEDED(result));
+
+	//テクスチャを赤クリア
+	{
+		//画素数
+		const UINT pixelCount = WinApp::window_width * WinApp::window_height;
+		//画像1行分のデータサイズ
+		const UINT rowPitch = sizeof(UINT) * WinApp::window_width;
+		//画像全体のデータサイズ
+		const UINT depthPitch = rowPitch * WinApp::window_height;
+		//画像イメージ
+		UINT* img = new UINT[pixelCount];
+		for (int i = 0; i < pixelCount; i++) {
+			img[i] = 0xff0000ff;
+		}
+		//テクスチャバッファデータ転送
+		result = texBuff->WriteToSubresource(0, nullptr, img, rowPitch, depthPitch);
+		assert(SUCCEEDED(result));
+		delete[] img;
+	}
+
+	//SRV用デスクリプタヒープ設定
+	D3D12_DESCRIPTOR_HEAP_DESC srvDescHeapDesc = {};
+	srvDescHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	srvDescHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	srvDescHeapDesc.NumDescriptors = 1;
+	//SRV用デスクリプタヒープ生成
+	result = dxCommon.GetDevice()->CreateDescriptorHeap(&srvDescHeapDesc, IID_PPV_ARGS(&srvHeap));
+	assert(SUCCEEDED(result));
+
+	//SRV設定
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
+	srvDesc.Texture2D.MipLevels = 1;
+
+	//デスクリプタヒープをSRV作成
+	dxCommon.GetDevice()->CreateShaderResourceView(texBuff.Get(),
+		&srvDesc,
+		srvHeap->GetCPUDescriptorHandleForHeapStart());
 }
 
 void PostEffect::Draw()
--- PostEffect.h
+++ PostEffect.h
@@ -10,9 +10,20 @@ public:
     PostEffect();
 
     /// <summary>
+    /// 初期化
+    /// </summary>
+    void initialize(DirectXCommon dxCommon);
+
+    /// <summary>
     /// 描画コマンドの実行
     /// </summary>
     /// <param name="cmdList"></param>
     void Draw();
+
+private:
+    // テクスチャバッファ
+    Microsoft::WRL::ComPtr<ID3D12Resource> texBuff;
+    // SRV用テクスチャヒープ
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvHeap;
 };
 
