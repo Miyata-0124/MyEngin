b94534a78c89170981e13ce2ca29ec2495b20e2c
MiyataKouki <k021g1124@g.neec.ac.jp>
2023-08-03 00:22:02 +0900

    評価2課題完了?

--- Resources/hlsl/PostEffectPS.hlsl
+++ Resources/hlsl/PostEffectPS.hlsl
@@ -1,19 +1,36 @@
 #include"PostEffect.hlsli"
-
 Texture2D<float4> tex0 : register(t0);
 Texture2D<float4> tex1 : register(t1);
 SamplerState smp : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-   float4 texcolor0 = tex0.Sample(smp, input.uv);
-   float4 texcolor1 = tex1.Sample(smp, input.uv);
+    float4 texcolor0 = tex0.Sample(smp, input.uv);
 
-   float4 color = texcolor0;
-   if (fmod(input.uv.y, 0.1f) < 0.05f)
-   {
-       color = texcolor1;
-   }
+    float shiftWidth = 0.005;
 
-   return float4(color.rgb, 1);
+    float4 texcolor1 = { 0, 0, 0, 0 };
+    texcolor1 += tex1.Sample(smp, input.uv + (-shiftWidth, -shiftWidth));
+    texcolor1 += tex1.Sample(smp, input.uv + (0, -shiftWidth));
+    texcolor1 += tex1.Sample(smp, input.uv + (+shiftWidth, -shiftWidth));
+
+    texcolor1 += tex1.Sample(smp, input.uv + (-shiftWidth, 0));
+    texcolor1 += tex1.Sample(smp, input.uv + (0, 0));
+    texcolor1 += tex1.Sample(smp, input.uv + (+shiftWidth, 0));
+
+    texcolor1 += tex1.Sample(smp, input.uv + (-shiftWidth, +shiftWidth));
+    texcolor1 += tex1.Sample(smp, input.uv + (0, +shiftWidth));
+    texcolor1 += tex1.Sample(smp, input.uv + (+shiftWidth, +shiftWidth));
+
+    texcolor1.rgb = texcolor1.rgb / 9;
+    texcolor1.a = 1;
+
+    float4 color = texcolor1;
+
+    if (fmod(input.uv.y, 0.1f) < 0.05f)
+    {
+        color = float4(1 - texcolor0.rgb, 1);
+    }
+
+    return color;
 }
