9177433659c463e60890063c6eb1226b02de55d0
MiyataKouki <k021g1124@g.neec.ac.jp>
2023-09-17 20:41:28 +0900

    敵のクラス

---
+++ enemy.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Object3D.h"
+
+class Enemy : public Object3d
+{
+public:
+	/// <summary>
+	/// オブジェクト生成
+	/// </summary>
+	/// <param name="model"></param>
+	/// <returns></returns>
+	static Enemy* Create(Model* model = nullptr);
+
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <returns></returns>
+	bool Initialize() override;
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update()override;
+
+	/// <summary>
+	/// コールバック
+	/// </summary>
+	/// <param name="info"></param>
+	void OnCollider(const CollisionInfo& info)override;
+};
+
