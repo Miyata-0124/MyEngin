d77ba1aa56b56a52573761091ae3fc5147dcb206
MiyataKouki <k021g1124@g.neec.ac.jp>
2023-11-14 16:44:54 +0900

    GamePlayからGameScene.hを手動でうつした

--- header/Game/GameScene.h
+++ header/Game/GameScene.h
@@ -6,33 +6,22 @@
  */
 
 #pragma once
-//自作クラス
+ //自作クラス
 #include <list>
 #include "header/Engin/Input.h"
 #include "header/Engin/WinApp.h"
 #include "header//Engin/DirectXCommon.h"
 #include "header/Engin/ViewProjection.h"
 #include "header/2D/SpriteCommon.h"
-#include "header/2D/Sprite.h"
 #include "header/3D/Object3D.h"
 #include "header/3D/FbxObject3D.h"
 #include "header/3D/FbxModel.h"
 #include "header/3D/Model.h"
 #include "header/3D/JsonLoader.h"
-#include "header/Game/BackGround.h"
-#include "header/Game/BlackOut.h"
-#include "header/Game/TitleSprite.h"
-#include "header/Game/OverSprite.h"
-#include "header/Game/ClearSprite.h"
-
-class CollisionManager;
-class Player;
-class BackGround;
-class Floor;
-class Item;
-class Wall;
-class Enemy;
-class Rain;
+//シーン関連
+#include "header/Game/GameSceneManager.h"
+#include "header/Game/GameTitleScene.h"
+#include "header/Game/GamePlayScene.h"
 
 class GameScene
 {
@@ -65,48 +54,27 @@ public: //mainに一部引き渡し用
 
 private: //ゲーム内使用クラス
 	WinApp* winApp = nullptr;
-	DirectXCommon* directXCom = nullptr;
+	DirectXCommon* directXCom = DirectXCommon::GetInstance();
 	//デバッグテキスト
 	//ImguiManager* imgui = nullptr;
 	//キー情報
 	Input* input = nullptr;
 	//カメラ
 	ViewProjection* camera = nullptr;
-	//スプライト
-	SpriteCommon* spriteCommon = nullptr;
 
-	TitleSprite* titleSprite = new TitleSprite();
-	OverSprite* overSprite = new OverSprite();
-	ClearSprite* clearSprite = new ClearSprite();
-	BlackOut* blackOut = new BlackOut();
 	//FBX
 	FbxModel* model = nullptr;
 	FbxObject3d* object1 = new FbxObject3d();
-	
+
+	//シーンマネージャ
+	GameSceneManager* sceneManager = nullptr;
 	//マップ用JsonLoader
 	//JSON
 	LevelData* jsonLoader = nullptr;
 	std::map<std::string, Model*> models;
 	std::vector<Object3d*> objects;
 
-	//プレイヤー
-	Player* objPlayer = nullptr;
-	//敵
-	Enemy* objEnem = nullptr;
-	//床(仮)
-	Floor* objFloor = nullptr;
-	//アイテム
-	Item* objItem = nullptr;
-	//壁
-	Wall* objWall = nullptr;
-	//背景
-	BackGround* objBackGround = nullptr;
 
-	CollisionManager* collisionManager = nullptr;
-
-	//パーティクル
-	//Particle* particle = nullptr;
-	Rain* rain = nullptr;
 	//シーン切り替え要情報
 	int scene = 0;
 	//雨を管理する時計
@@ -114,6 +82,7 @@ private: //ゲーム内使用クラス
 	bool rainFlag = false;
 	////UI関連
 	bool isBlackOut = false;
-	
+
+	GameBaseScene* scene_ = nullptr;
 };
 
