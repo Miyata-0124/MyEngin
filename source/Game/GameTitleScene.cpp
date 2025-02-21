a240811c74ca4d67779762bea33599b8a16e9e47
Miyata-0124 <94036166+Miyata-0124@users.noreply.github.com>
2025-02-22 03:32:38 +0900

    初期状態に戻した

--- source/Game/GameTitleScene.cpp
+++ source/Game/GameTitleScene.cpp
@@ -3,13 +3,13 @@
 
 void GameTitleScene::Initialize(ViewProjection* camera_,Input* input_)
 {
+	//カメラ情報
 	camera = camera_;
-	input = input_;
 	//キー情報
 	input = input_;
 	//スプライト共通部分の初期化
 	spriteCommon->Initialize(directXCom);
-	//spriteCommon->Loadtexture(1, "fileName.png");
+	spriteCommon->Loadtexture(1, "black1x1.png");
 	//一度だけ宣言する
 	//Particle::StaticInitialize(directXCom->GetDevice(), camera_);
 
